b6e62f2f32a868018da8fffb97887eaeff5d41e6
Bilalov_Bulat <bulat-cloud-1@yandex.ru>
2020-07-21 12:22:02 +0300

    #7 Uploading files to the server and distributing statics

--- Sweater/src/main/java/ru/itis/controller/MainController.java
+++ Sweater/src/main/java/ru/itis/controller/MainController.java
@@ -1,20 +1,29 @@
 package ru.itis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.itis.model.Message;
 import ru.itis.model.User;
 import ru.itis.repository.MessageRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     private final MessageRepository messageRepository;
 
@@ -47,15 +56,25 @@ public class MainController {
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Map<String, Object> model){
-        messageRepository.save(Message.builder()
-                .text(text)
-                .tag(tag)
-                .author(user)
-                .build());
+                      Map<String, Object> model,
+                      @RequestParam("file") MultipartFile file) throws IOException {
+        Message message = new Message();
+        message.setText(text);
+        message.setTag(tag);
+        message.setAuthor(user);
+        if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()){
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(
+                    uploadPath + "/" + resultFilename));
+        }
+        messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
         return "main";
     }
 }
-
--- Sweater/src/main/resources/templates/registration.ftl
+++ Sweater/src/main/resources/templates/registration.ftl
@@ -3,6 +3,6 @@
 
 <@c.page>
 Add new user
-${message!" "}
+${message!}
 <@l.login path="/registration"/>
 </@c.page>
