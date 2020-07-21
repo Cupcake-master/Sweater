c20b01982af411695a01a23a2d458aa8f53110be
Bilalov_Bulat <bulat-cloud-1@yandex.ru>
2020-07-21 10:48:19 +0300

    #6 Add the admin panel and user roles, restrict access

--- Sweater/src/main/java/ru/itis/controller/UserController.java
+++ Sweater/src/main/java/ru/itis/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.itis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     private final UserRepository userRepository;
