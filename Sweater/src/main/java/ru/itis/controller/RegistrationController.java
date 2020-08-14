d948e7ffa9d0cd25bdf249cb2cc08915da35864b
Cupcake-master <Bulat-cloud-1@yandex.ru>
2020-08-14 12:33:14 +0300

    #9 Sending mail to users, activating an account

--- Sweater/src/main/java/ru/itis/controller/RegistrationController.java
+++ Sweater/src/main/java/ru/itis/controller/RegistrationController.java
@@ -2,23 +2,23 @@ package ru.itis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.itis.model.Role;
 import ru.itis.model.User;
-import ru.itis.repository.UserRepository;
+import ru.itis.service.UserService;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/registration")
@@ -28,14 +28,21 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String registration(User user, Map<String, Object> model){
-        User someUser = userRepository.findByUsername(user.getUsername());
-        if (someUser != null){
+        if (!userService.addUser(user)){
             model.put("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated");
+        }else{
+            model.addAttribute("message", "Activation code is not found");
+        }
+        return "login";
     }
 }
--- Sweater/src/main/java/ru/itis/repository/UserRepository.java
+++ Sweater/src/main/java/ru/itis/repository/UserRepository.java
@@ -6,4 +6,6 @@ import ru.itis.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findById(String id);
+
+    User findByActivationCode(String code);
 }
